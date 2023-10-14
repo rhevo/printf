942052b2f0e4012adf2ed83b9986ac971d0a9a0b
rhevo <eugenenarh1@gmail.com>
2023-10-14 15:31:44 -0700

    initial push

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H 
+#define MAIN_H
+
+#include <limits.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+
+
+
+int _printf(const char *format, ...);
+
+#endif
