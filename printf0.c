736ebf64a5a8ef3f1b611fc8e9f3f2d5f09b7b48
rhevo <eugenenarh1@gmail.com>
2023-10-14 16:17:11 -0700

    d&i

--- printf0.c
+++ printf0.c
@@ -42,6 +42,18 @@ int _printf(const char *format,
 				write(1, &c, 1);
 				c_pri++;
 			}
+			else if (*format == 'd')
+                	{
+                        	d = va_arg(pars, int);
+                        	write(1, &d, 1);
+                        	c_pri++;
+                	}
+                	else if (*format == 'i')
+                	{
+                        	i = va_arg(pars, int);
+                        	write(1, &i, 1);
+                        	c_pri++;
+                	}
 			else if (*format == 's')
 			{
 				s = va_arg(pars, char *);
